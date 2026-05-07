e9dfd228d6d7c14947cba001a35c03903792d3cc
skashyap08 <sakshikashyap9876@gmail.com>
2026-05-07 16:19:45 +0530

    updating files

--- app.py
+++ app.py
@@ -101,5 +101,6 @@ def schedule():
 
 # Run app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
